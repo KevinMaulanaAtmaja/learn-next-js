7dfeabac0c6b2036bbc921180f237444de7d0b29
Kevin Maulana Atmaja <kevinmaulanaat@gmail.com>
2025-05-23 12:00:00 +0700

    link & navigation

--- next-app-router/src/app/layout.tsx
+++ next-app-router/src/app/layout.tsx
@@ -3,21 +3,32 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "./navbar";
 import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
+const disabledNavbar = ["/login", "/register"];
+const allowCount = ["/", "/about", "/about/profile"];
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     const [state, setState] = useState(0);
+    const pathname = usePathname();
 
     return (
         <html lang="en">
             <body className={inter.className}>
-                <Navbar />
+                {!disabledNavbar.includes(pathname) && <Navbar />}
                 <div className="mx-5">
-                    <h1>Layout: {state}</h1>
-                    <button className="bg-blue-300  border border-b-blue-900 py-1 px-8" onClick={() => setState(state + 1)}>
-                        Klik
-                    </button>
+                    {allowCount.includes(pathname) && (
+                        <>
+                            <h1>Layout: {state}</h1>
+                            <button
+                                className="bg-blue-300  border border-b-blue-900 py-1 px-8"
+                                onClick={() => setState(state + 1)}
+                            >
+                                Klik
+                            </button>
+                        </>
+                    )}
                     {children}
                 </div>
             </body>
--- next-app-router/src/app/template.tsx
+++ next-app-router/src/app/template.tsx
@@ -1,14 +1,27 @@
 "use client";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
+
+const allowCount = ["/", "/about", "/about/profile"];
 
 export default function template({ children }: { children: React.ReactNode }) {
     const [state, setState] = useState(0);
+    const pathname = usePathname();
+
     return (
         <div>
-            <h1>Template: {state}</h1>
-            <button className="bg-emerald-300  border border-b-bg-emerald-900 py-1 px-8" onClick={() => setState(state + 1)}>
-                Klik
-            </button>
+            {allowCount.includes(pathname) && (
+                <>
+                    <h1>Template: {state}</h1>
+                    <button
+                        className="bg-emerald-300  border border-b-bg-emerald-900 py-1 px-8"
+                        onClick={() => setState(state + 1)}
+                    >
+                        Klik
+                    </button>
+                </>
+            )}
+
             {children}
         </div>
     );
